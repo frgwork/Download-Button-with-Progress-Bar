43ab465b227299bdff3815fc90ffe4feffd95132
frgwork <frgwork@ya.ru>
2022-04-15 22:52:32 +0300

    Download Button with Progress Bar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+let button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+   button.classList.add('active');
+
+   setTimeout(() => {
+      button.classList.remove('active');
+      document.querySelector('i').classList.replace('bx-cloud-download', 'bx-check-circle');
+      document.querySelector('.button-text').innerText = "Completed";
+
+   }, 6000)
+})
